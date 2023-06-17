b7dd10ec2cee53c5a2b120ca03676d3880fc473a
Spydy-Coder <81745446+Spydy-Coder@users.noreply.github.com>
2023-06-17 09:30:47 +0530

    removing / from origin in socket.io

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "https://resolver-vercel-front.vercel.app/",
+    origin: "https://resolver-vercel-front.vercel.app",
     credentials: true,
   },
 });
